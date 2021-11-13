650938efc8af43b2437b66021f3acf2197418c18
Jeff <linkthedestroyer@gmail.com>
2021-11-12 22:16:08 -0600

    update

--- core/urls.py
+++ core/urls.py
@@ -6,9 +6,7 @@ from inventory.views import *
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("inventory/", UserInventoryView.as_view(), name="user_inventory"),
-    path(
-        "inventory/edit", UpdateUserInventoryView.as_view(), name="edit_user_inventory"
-    ),
+    path("inventory/edit", UpdateUserInventoryView.as_view(), name="edit_user_inventory"),
     path(
         "card/search",
         TemplateView.as_view(template_name="cards\card_search.html"),
